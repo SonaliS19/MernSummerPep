d1f966f4c7692ce2ea4c9d91e5b63cea1532513f
SibbuS <sonali.sibbu@gmail.com>
2024-06-11 15:30:00 +0530

    "update"

--- JavaScript/notes.md
+++ JavaScript/notes.md
@@ -1,4 +1,4 @@
 //variable in js
--var
--let
--const
+-var(scope:fuctional, re-assingment and re-decleration is allowed)
+-let(scope:local to block, re-assingment is allowed but  re-decleration is not allowed)
+-const(re-assingment and re-decleration is not allowed)
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,21 @@
 console.log("working")
+
+//decleration
+var age;
+//assingment/initialization
+age=19;
+console.log(age);
+//re-assingment
+age= 20;
+console.log(age);
+
+// function abc(){
+//   var username="Sonali";
+//   console.log(username);
+// }
+
+let username = "ABC"
+console.log(username);
+
+username ="XYZ"
+console.log(username);
